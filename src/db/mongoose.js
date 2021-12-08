f2d1d603726572294957910822a2304ed5240c26
Archit <kaushalarchit5@gmail.com>
2021-12-08 12:22:23 +0530

    adds cloud database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/Railgadi', {
+mongoose.connect('mongodb+srv://archit:Mongopass@cluster0.wlsiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
