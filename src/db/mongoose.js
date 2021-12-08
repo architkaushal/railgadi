c60c0fe1eea4702df85b78f0a4a2e6a675b741ba
Archit <kaushalarchit5@gmail.com>
2021-12-08 13:44:34 +0530

    adds cloud atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,3 +5,30 @@ mongoose.connect('mongodb+srv://archit:Mongopass@cluster0.wlsiu.mongodb.net/rail
     useNewUrlParser: true,
     useCreateIndex: true
 })
+
+{
+    "_id": {
+        "$oid": "61b05ecbb0543898a4443466"
+    },
+    
+    "trName" : "Majra Express",
+    "trNo" : 20,
+    "start" : "Ahmedabad",
+    "end" : "New Delhi",
+    "runsOn" : [ 
+        {
+            "_id" : ObjectId("61b05e53b0543898a4443463"),
+            "days" : "Wednesday"
+        }, 
+        {
+            "_id" : ObjectId("61b05e91b0543898a4443465"),
+            "days" : "Monday"
+        }, 
+        {
+            "_id" : ObjectId("61b05e75b0543898a4443464"),
+            "days" : "Sunday"
+        }
+    ]
+   
+
+}
