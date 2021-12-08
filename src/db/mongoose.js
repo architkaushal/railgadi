931121b178982eb1d66b492e713b603bbe832d87
Archit <kaushalarchit5@gmail.com>
2021-12-08 13:49:13 +0530

    resolves bug

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,29 +6,3 @@ mongoose.connect('mongodb+srv://archit:Mongopass@cluster0.wlsiu.mongodb.net/rail
     useCreateIndex: true
 })
 
-{
-    "_id": {
-        "$oid": "61b05ecbb0543898a4443466"
-    },
-    
-    "trName" : "Majra Express",
-    "trNo" : 20,
-    "start" : "Ahmedabad",
-    "end" : "New Delhi",
-    "runsOn" : [ 
-        {
-            "_id" : ObjectId("61b05e53b0543898a4443463"),
-            "days" : "Wednesday"
-        }, 
-        {
-            "_id" : ObjectId("61b05e91b0543898a4443465"),
-            "days" : "Monday"
-        }, 
-        {
-            "_id" : ObjectId("61b05e75b0543898a4443464"),
-            "days" : "Sunday"
-        }
-    ]
-   
-
-}
